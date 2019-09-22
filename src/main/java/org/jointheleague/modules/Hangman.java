1d25faa0973c93720f87ffa5c4e0513869c7337a
League Student <league@iMac-12.attlocal.net>
2019-09-21 17:27:19 -0700

    21 September 2019

--- src/main/java/org/jointheleague/modules/Hangman.java
+++ src/main/java/org/jointheleague/modules/Hangman.java
@@ -8,13 +8,13 @@ public class Hangman extends CustomMessageCreateListener {
 
 	private String current_guess;
 	private String answer_string;
-	private int num_incorrect = 0;
+	private int num_incorrect;
 	
+	private ArrayList<Character> good_guesses;
 	private ArrayList<Character> wrong_guesses = new ArrayList<Character>();
 	
-	private boolean GAME_RUNNING = false;
-	
 	private final int NUM_MAX = 6;
+	private boolean GAME_RUNNING = false;
 	
 	
 	public Hangman(String channelName) {
@@ -24,71 +24,95 @@ public class Hangman extends CustomMessageCreateListener {
 	@Override
 	public void handle(MessageCreateEvent event) {
 		if (event.getMessageContent().contains("!begin hangman ")) {
-			answer_string = event.getMessageContent().replace("!begin hangman ", "");
-			event.deleteMessage();
-			current_guess = generateUnknown(answer_string);
-			GAME_RUNNING = true;
-			event.getChannel().sendMessage("A hangman game has started! The format is " + current_guess + ". Begin guessing now!");
+			createGame(event);
 		}
 		
 		if (event.getMessageContent().contains("!guess ") && GAME_RUNNING) {
-			String guess = event.getMessageContent().replace("!begin hangman ", "");
-			if(guess.length() != 1) {event.deleteMessage();}
-			else
-			{
-				boolean was_correct = false;
-				char c_guess = guess.charAt(0);
-				
-				for(int i = 0; i < answer_string.length(); i++)
-				{
-					if(answer_string.charAt(i) == c_guess) {
-						current_guess = current_guess.substring(0, i) + c_guess + current_guess.substring(i + 1, answer_string.length());
-						was_correct = true;
-					}
-				}
-				
-				if(was_correct) {
-					event.getChannel().sendMessage("The guess of " + c_guess + " was correct! The word is now " + current_guess + ".");
-					checkGameState(event);
-				}
-				
-				else if(!wrong_guesses.contains(c_guess))
-				{
-					num_incorrect++;
-					event.getChannel().sendMessage("The guess of " + c_guess + " was incorrect!" + num_incorrect + " guesses remain.");
-					checkGameState(event);
-				}
-				else
-				{
-					event.deleteMessage();
-				}
-			}
+			guess(event);
 		}
 		
 		if (event.getMessageContent().contains("!see guesses") && GAME_RUNNING) {
-			event.deleteMessage();
-			event.getChannel().sendMessage("The current incorrect guesses are " + wrong_guesses);
+			event.getChannel().sendMessage("The current incorrect guesses are " + wrong_guesses.toString().substring(1, wrong_guesses.toString().length() - 1));
 		}
 		
 		if (event.getMessageContent().contains("!see word") && GAME_RUNNING) {
-			event.deleteMessage();
 			event.getChannel().sendMessage("The currently known part of the word is " + current_guess);
+		}
+	}
+	
+	public void createGame(MessageCreateEvent event)
+	{
+		
+		if(!GAME_RUNNING) {
+			good_guesses = new ArrayList<Character>();
+			wrong_guesses = new ArrayList<Character>();
+			
+			answer_string = event.getMessageContent().replace("!begin hangman ", "");
+			current_guess = generateUnknown(answer_string);
+			
+			num_incorrect = 0;
+			GAME_RUNNING = true;
+			
+			event.editMessage("!begin hangman");
+			
+			event.getChannel().sendMessage("A hangman game has started! The format is " + current_guess + ". Begin guessing now!");	
+		}
+			
+		else {
+			event.editMessage("!begin hangman");
+			event.getChannel().sendMessage("A game is already being played.");
+		}
+	}
+	
+	public void guess(MessageCreateEvent event)
+	{
+		String guess = event.getMessageContent().replace("!guess ", "");
+		if(guess.length() == 1)
+		{
+			boolean was_correct = false;
+			char c_guess = guess.charAt(0);
+			
+			for(int i = 0; i < answer_string.length(); i++)
+			{
+				if(answer_string.charAt(i) == c_guess) {
+					current_guess = current_guess.substring(0, i) + c_guess + current_guess.substring(i + 1, answer_string.length());
+					was_correct = true;
+				}
+			}
+			
+			if(was_correct && !good_guesses.contains(c_guess)) {
+					event.getChannel().sendMessage("The guess of " + c_guess + " is correct!");
+					good_guesses.add(c_guess);
+					checkGameState(event);
+			}
+			
+			else if(!wrong_guesses.contains(c_guess))
+			{
+				num_incorrect++;
+				wrong_guesses.add(c_guess);
+				event.getChannel().sendMessage("The guess of " + c_guess + " is incorrect! " + (NUM_MAX - num_incorrect) + " incorrect guesses remain.");
+				checkGameState(event);
+			}
+			
+			else if(wrong_guesses.contains(c_guess) || good_guesses.contains(c_guess))
+			{
+				event.getChannel().sendMessage("The character \'" + c_guess + "\' has already been guessed.");
+			}
 		}
 	}
 	
 	public void checkGameState(MessageCreateEvent event)
 	{
 		if(answer_string.equals(current_guess)) {
-			event.getChannel().sendMessage("The word has been guessed! It was " + answer_string);
-			event.deleteMessage();
-			GAME_RUNNING = false;
-			}
-		
-		else if (num_incorrect == NUM_MAX) {
-			event.getChannel().sendMessage("The game is lost! The word was " + answer_string);
-			event.deleteMessage();
+			event.getChannel().sendMessage("The word \"" + answer_string + "\" is correct!");
 			GAME_RUNNING = false;
 		}
+		
+		else if (num_incorrect >= NUM_MAX) {
+			event.getChannel().sendMessage("The game is lost! The word was \"" + answer_string + "\".");
+			GAME_RUNNING = false;
+		}
+		
 	}
 	
 	public String generateUnknown(String s)
@@ -97,7 +121,7 @@ public class Hangman extends CustomMessageCreateListener {
 		for(char c: s.toCharArray())
 		{
 			if(c == ' ') {ret = ret + " ";}
-			else {ret = ret + "_";}
+			else {ret = ret + "-";}
 		}
 		
 		return ret;
